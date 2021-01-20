ade26901621f516956eb7dd8050eeea04fe369ad
Daniel <d.spinardi199@gmail.com>
2021-01-20 14:45:51 -0300

    Fix env helper add default var value

--- core/helpers.php
+++ core/helpers.php
@@ -5,8 +5,11 @@ if (!function_exists('env')) {
      * @param string $var
      * @return string
      */
-    function env(string $var): string
+    function env(string $var, ?string $defaultVarValue = null): ?string
     {
-        return $_ENV[$var] ?? null;
+        if (isset($_ENV[$var]) and $_ENV[$var] != null)
+            return $_ENV[$var];
+
+        return $defaultVarValue;
     }
 }
