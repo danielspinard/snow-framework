bcafdb2c232e47f9a5df3ea0c4262845df1e3161
Daniel <d.spinardi199@gmail.com>
2021-01-21 00:08:23 -0300

    Add model business rule

--- app/Models/PostModel.php
+++ app/Models/PostModel.php
@@ -9,12 +9,47 @@ class PostModel extends DataLayer
     public function __construct()
     {
         parent::__construct(
-            "posts", //string "TABLE_NAME"
+            "posts",
             [
-                "title", "content" // array "REQUIRED_FIELD"
+                "title", "content"
             ], 
-            "post_id", // string "PRIMARY_KEY"
-            false // bool "TIMESTAMPS"
+            "post_id",
+            false
         );
+    }
+
+    public function save(): bool
+    {
+        if(
+            !$this->validateTitle() 
+            or !$this->validateContent() 
+            or !parent::save()
+        )
+
+        return true;
+    }
+
+    private function validateTitle(): bool
+    {
+        if(
+            strlen($this->title) > 5
+            and strlen($this->title) < 50
+        )
+            return true;
+
+        $this->fail = new Exception("The title of the post must contain between 5 and 50 characters!");
+        return false;
+    }
+
+    private function validateContent(): bool
+    {
+        if(
+            strlen($this->content) > 5
+            and strlen($this->title) < 255
+        )
+            return true;
+
+        $this->fail = new Exception("The content of the post must contain between 5 and 255 characters!");
+        return false;
     }
 }
