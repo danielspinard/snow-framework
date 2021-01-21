e6780f5d098aa12a22603a4fba867f1683f4d3ce
Daniel <d.spinardi199@gmail.com>
2021-01-20 22:55:54 -0300

    Fix path to views, compile and fix render return method

--- core/Engine.php
+++ core/Engine.php
@@ -14,20 +14,20 @@ class Engine
     public function __construct()
     {
         $this->blade = new Blade(
-            dirname(__DIR__, 1) . "resources/",
-            dirname(__DIR__, 1) . "runtime/compile/",
+            dirname(__DIR__, 1) . "/resources",
+            dirname(__DIR__, 1) . "/runtime/compile",
         );
 
         $this->blade->setBaseUrl(env('APP_URL'));
         $this->blade->setFileExtension(".blade.php");
-        $this->blade->setMode((appDebug())? Blade::MODE_SLOW : Blade::MODE_FAST);
+        $this->blade->setMode((appDebug())? Blade::MODE_DEBUG : Blade::MODE_FAST);
         $this->blade->pipeEnable = true;
 
         return $this;
     }
 
-    public function render(string $view, ?array $data): void
+    public function render(string $view, ?array $data)
     {
-        echo $this->blade->setView($view)->share($data)->run();
+        return $this->blade->setView($view)->share($data)->run();
     }
 }
