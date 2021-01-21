9571d83f0737d6fca065ea5ac67bc363a6f515da
Daniel <d.spinardi199@gmail.com>
2021-01-20 23:45:06 -0300

    Fix posts get by model

--- app/Controllers/WebController.php
+++ app/Controllers/WebController.php
@@ -8,9 +8,11 @@ class WebController
 {
     public function debug()
     {
+        $posts = (new PostModel())->find()->order('title')->fetch(true) ?? [];
+        
         return view('welcome', [
             'titlePage' => 'Hello, my first view',
-            'posts' => (new PostModel())->find()->order('title')->fetch(true)
+            'posts' => $posts
         ]);
     }
 }
