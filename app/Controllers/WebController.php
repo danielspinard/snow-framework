858dab7986c12a5b339ac990c7df327c176239af
Daniel <d.spinardi199@gmail.com>
2021-01-20 23:33:35 -0300

    Fix pagetitle and posts

--- app/Controllers/WebController.php
+++ app/Controllers/WebController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controllers;
 
+use App\Models\PostModel;
+
 class WebController
 {
     public function debug()
     {
-        return view('welcome');
+        return view('welcome', [
+            'titlePage' => 'Hello, my first view',
+            'posts' => (new PostModel())->find()->order('title')->fetch(true)
+        ]);
     }
 }
