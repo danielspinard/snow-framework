cad2b5eab4bcf1006adc21468b97e905bd910104
Daniel <d.spinardi199@gmail.com>
2021-01-23 08:01:25 -0300

    Fix getModel and getFacade methods

--- core/Container.php
+++ core/Container.php
@@ -15,4 +15,30 @@ class Container
     {
         return class_exists($class);
     }
+
+    /**
+     * @param string $class
+     * @param string $namespace
+     * @return object|null
+     */
+    public static function getModel(string $class, ?string $namespace = '\\App\\Models\\'): ?object
+    {
+        if(self::classExists($model = $namespace . ucfirst($class)))
+            return new $model();
+        
+        return null;
+    }
+
+    /**
+     * @param string $class
+     * @param string $namespace
+     * @return object|null
+     */
+    public static function getFacade(string $class, ?string $namespace = '\\App\\Facades\\'): ?object
+    {
+        if(self::classExists($facade = $namespace . ucfirst($class)))
+            return new $facade();
+        
+        return null;
+    }
 }
