fce39d3f16b05caaa41ed744e61b87339187d8e2
Daniel <d.spinardi199@gmail.com>
2021-02-22 04:51:27 -0300

    Delete web route debug

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,3 @@
 $router->namespace("App\Controllers")->group(null);
 $router->get("/", "WebController@index");
 $router->get("/{id}", "WebController@show");
-$router->get("/debug", "WebController@debug", "web.debug");
