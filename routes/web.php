c867223d6194fbc9ee59ec5a490db16806d96683
Daniel <d.spinardi199@gmail.com>
2021-02-22 04:50:25 -0300

    Add show route

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,5 @@
 
 $router->namespace("App\Controllers")->group(null);
 $router->get("/", "WebController@index");
+$router->get("/{id}", "WebController@show");
 $router->get("/debug", "WebController@debug", "web.debug");
