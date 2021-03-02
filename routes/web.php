1edfd691d66734ce59a4c28820dcfa8ef2edce77
Daniel <d.spinardi199@gmail.com>
2021-03-01 23:09:39 -0300

    Fix named routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-$router->namespace("App\Controllers")->group(null);
-$router->get("/", "WebController@index");
-$router->get("/{id}", "WebController@show");
+$router->namespace('App\Controllers')->group(null);
+$router->get('/', 'WebController@index', 'card.index');
+$router->get('/{id}', 'WebController@show', 'card.show');
