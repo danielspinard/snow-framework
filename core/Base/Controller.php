18f2e8eb151815b768d28ff5a00da2ec70eb5a92
Daniel <d.spinardi199@gmail.com>
2021-03-01 23:18:45 -0300

    Fix docblock and add redirect method

--- core/Base/Controller.php
+++ core/Base/Controller.php
@@ -3,17 +3,28 @@
 namespace Snow\Base;
 
 use Snow\Engine;
+use Snow\Redirect;
 
 abstract class Controller
 {
     /**
      * @param string $view
-     * @param array|null $data
+     * @param array $data
      * @param string $customPath
      * @return void
      */
-    protected function render(string $view, ?array $data = [], string $customPath = 'views'): void
+    protected function render(string $view, array $data = [], string $customPath = 'views'): void
     {
-        echo (new Engine)->render($customPath . '.' . $view, $data);
+        echo (new Engine())->render($customPath . '.' . $view, $data);
+    }
+
+    /**
+     * @param string $route
+     * @param array $data
+     * @return void
+     */
+    protected function redirect(string $route, array $data = [])
+    {
+        Redirect::route($route, $data)::run();
     }
 }
