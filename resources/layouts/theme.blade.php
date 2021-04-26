1d02f63ab181cd322bf46afc0359082dd34637fa
Daniel <d.spinardi199@gmail.com>
2021-04-26 17:21:50 -0300

    Fix framework css folder and layout style section

--- resources/layouts/theme.blade.php
+++ resources/layouts/theme.blade.php
@@ -4,16 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link 
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-        crossorigin="anonymous"
-    >
-
-    @stack('style')
-
     <title>{{ $title_page ?? env('APP_NAME') }}</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" crossorigin="anonymous">
+    @stack('style')
 </head>
 
 <body>
@@ -21,12 +14,7 @@
         @yield('content')
     </div>
 
-    <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-        crossorigin="anonymous"
-    >
-    </script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     @stack('script')
 </body>
 
