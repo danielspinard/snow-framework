02892386192ae04170d02e53a2a32009274feea5
Daniel <d.spinardi199@gmail.com>
2021-04-26 17:27:20 -0300

    Fix code smell "Control structures should use curly braces"

--- app/Models/PostModel.php
+++ app/Models/PostModel.php
@@ -7,6 +7,9 @@ use Exception;
 
 class PostModel extends DataLayer
 {
+    /**
+     * DataLayer constructor.
+     */
     public function __construct()
     {
         parent::__construct(
@@ -19,39 +22,45 @@ class PostModel extends DataLayer
         );
     }
 
+    /**
+     * @return bool
+     */
     public function save(): bool
     {
         if(
             !$this->validateTitle() 
             || !$this->validateContent() 
             || !parent::save()
-        )
+        ) {
             return false;
+        }
             
         return true;
     }
 
+    /**
+     * @return bool
+     */
     private function validateTitle(): bool
     {
-        if(
-            strlen($this->title) > 5
-            && strlen($this->title) < 50
-        )
-            return true;
-
-        $this->fail = new Exception("The title of the post must contain between 5 and 50 characters!");
-        return false;
+        if(strlen($this->title) < 5 || strlen($this->title) > 50) {
+            $this->fail = new Exception("The title of the post must contain between 5 and 50 characters!");
+            return false;
+        }
+        
+        return true;
     }
 
+    /**
+     * @return bool
+     */
     private function validateContent(): bool
     {
-        if(
-            strlen($this->content) > 5
-            && strlen($this->content) < 255
-        )
-            return true;
+        if(strlen($this->content) < 5 || strlen($this->content) > 255) {
+            $this->fail = new Exception("The content of the post must contain between 5 and 255 characters!");
+            return false;
+        }
 
-        $this->fail = new Exception("The content of the post must contain between 5 and 255 characters!");
-        return false;
+        return true;
     }
 }
