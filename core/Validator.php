7d9adbb110aac65b9fc812331445ca7b583bf1c8
Daniel <d.spinardi199@gmail.com>
2021-04-26 18:48:11 -0300

    Add alphanumeric string validator

--- core/Validator.php
+++ core/Validator.php
@@ -2,6 +2,16 @@
 
 namespace Snow\Base;
 
+use function ctype_alnum;
+
 class Validator
 {
+    /**
+     * @param string $string
+     * @return bool
+     */
+    public static function alphanum(string $string): bool
+    {
+        return ctype_alnum($string);
+    }
 }
