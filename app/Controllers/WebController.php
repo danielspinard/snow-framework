d2dc0e7a6e179ca5ce3942ae382ef13699f3594d
Daniel <d.spinardi199@gmail.com>
2021-04-27 19:21:47 -0300

    Refactor WebController

--- app/Controllers/WebController.php
+++ app/Controllers/WebController.php
@@ -3,39 +3,46 @@
 namespace App\Controllers;
 
 use Snow\Base\Controller;
-use Snow\Bootstrap;
+use Snow\Uploader;
 use App\Models\PostModel;
 
 class WebController extends Controller
 {
+    /**
+     * @return void
+     */
     public function index()
     {
-        $posts = (new PostModel())->find()->order('title')->fetch(true) ?? [];
-
-        return $this->render('welcome', [
-            'posts' => $posts
+        return $this->render('post.index', [
+            'posts' => (new PostModel())->find()->fetch(true)
         ]);
     }
 
-    public function show(array $data)
-<<<<<<< HEAD
+    /**
+     * @return void
+     */
+    public function show()
     {
-        dd((new PostModel)->findById($data['id']));
+        $post = (new PostModel)->findById($this->request()->id);
+
+        if (!$post) {
+            return $this->redirect('app.index');
+        }
+
+        return $this->render('post.show', ['post' => $post->data()]);
     }
 
+    /**
+     * @return void
+     */
     public function debug()
     {
-        // dump(Bootstrap::$core);
-        Bootstrap::make('Minify', 'Snow\\');
-        Bootstrap::make('ExampleMIddleware', 'App\\MIddlewares\\');
-        // dump(Bootstrap::$core);
-        // Bootstrap::make('Test');
-        dump(Bootstrap::$core);
-        // ($this->redirect('app.show', ['id' => 404]));
-=======
-    {
-        $post = (new PostModel())->findById($data['id']);
-        dd($post);
->>>>>>> 18f2e8eb151815b768d28ff5a00da2ec70eb5a92
+        // save test
+        // $post = (new PostModel());
+        // $post->title;
+        // $post->content;
+        // $post->save();
+
+        // var_dump($post->fail());
     }
 }
