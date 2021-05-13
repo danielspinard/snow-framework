f680edf132421e15f94a344488d73282d193ce96
Daniel <d.spinardi199@gmail.com>
2021-05-13 18:30:34 -0300

    Add method to scan files in specified path

--- core/Minify.php
+++ core/Minify.php
@@ -2,6 +2,43 @@
 
 namespace Snow;
 
+use InvalidArgumentException;
+use DirectoryIterator;
+use MatthiasMullie\Minify\Minify as Minifier;
+
 class Minify
 {
+    /**
+     * @var array
+     */
+    private $basedir = [];
+
+    /**
+     * Minify constructor
+     * 
+     * @param string $basedirCSS
+     * @param string $basedirJS
+     */
+    public function __construct(string $basedirCSS, string $basedirJS)
+    {
+        $this->basedir['css'] = $basedirCSS;
+        $this->basedir['js'] = $basedirJS;
+    }
+
+    /**
+     * @param string $dirPath
+     * @return Minifier
+     */
+    private function scandir(Minifier $minifier, string $type): Minifier
+    {
+        $files = new DirectoryIterator($this->basedir[$type]);
+
+        foreach ($files as $file) {
+            if ($file->getExtension() === $type) {
+                $minifier->add($file->getPathname());
+            }
+        }
+
+        return $minifier;
+    }
 }
