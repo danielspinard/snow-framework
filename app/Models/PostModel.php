7e2c4d98c7d25e52c4efd44b1701515bd0e8fa5c
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-11 16:27:34 -0300

    Fix model syntax with Eloquent  model

--- app/Models/PostModel.php
+++ app/Models/PostModel.php
@@ -2,75 +2,29 @@
 
 namespace App\Models;
 
-use Exception;
-use CoffeeCode\DataLayer\DataLayer;
-use Snow\Validator;
+use Illuminate\Database\Eloquent\Model;
 
-class PostModel extends DataLayer
+class PostModel extends Model
 {
     /**
-     * DataLayer constructor.
+     * @var string
      */
-    public function __construct()
-    {
-        parent::__construct(
-            'posts',
-            [
-                'title', 'content'
-            ], 
-            'post_id',
-            false
-        );
-    }
+    protected $table = 'posts';
 
     /**
-     * @return bool
+     * @var string
      */
-    public function save(): bool
-    {
-        if (
-            !$this->validateTitle() 
-            || !$this->validateContent() 
-            || !parent::save()
-        ) {
-            return false;
-        }
-            
-        return true;
-    }
+    protected $primaryKey = 'post_id';
 
     /**
-     * @return bool
+     * @var array
      */
-    private function validateTitle(): bool
-    {
-        $rules = ['min' => 5, 'max' => 50];
-
-        if (!Validator::length($this->title, $rules['min'], $rules['max'])) {
-            $this->fail = new Exception('The title of the post must contain between ' . $rules['min'] . ' and ' . $rules['max'] . ' characters!');
-            return false;
-        }
-
-        if (!Validator::alphanum($this->title)) {
-            $this->fail = new Exception('The title cannot contain special characters!');
-            return false;
-        }
-        
-        return true;
-    }
+    protected $fillable = [
+        'title', 'content'
+    ];
 
     /**
-     * @return bool
+     * @var bool
      */
-    private function validateContent(): bool
-    {
-        $rules = ['min' => 5, 'max' => 255];
-
-        if (!Validator::length($this->content, $rules['min'], $rules['max'])) {
-            $this->fail = new Exception('The content of the post must contain between ' . $rules['min'] . ' and ' . $rules['max'] . ' characters!');
-            return false;
-        }
-
-        return true;
-    }
+    public $timestamps = false;
 }
