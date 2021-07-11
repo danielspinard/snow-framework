22ef78fc77ec896c0e3c4d184b54136cd595d8a7
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-11 16:26:07 -0300

    Fix host/port DatabaseManager settings

--- core/Database.php
+++ core/Database.php
@@ -6,36 +6,9 @@ namespace Snow;
 use PDO;
 use Exception;
 use Illuminate\Database\Capsule\Manager;
-use function define;
-use function in_array;
 
 class Database
 {
-    /**
-     * @var string
-     */
-    private $driver;
-
-    /**
-     * @var string
-     */
-    private $host;
-    
-    /**
-     * @var string
-     */
-    private $port;
-
-    /**
-     * @var string
-     */
-    private $name;
-
-    /**
-     * @var string
-     */
-    private $username;
-
     /**
      * @param bool $connect
      * @return void
@@ -45,13 +18,6 @@ class Database
         if (!$connect) {
             return;
         }
-
-        $this->driver = env('DB_DRIVER', 'mysql');
-        $this->host = env('DB_HOST', '127.0.0.1');
-        $this->port = env('DB_PORT', '3306');
-        $this->name = env('DB_DATABASE', 'test');
-        $this->username = env('DB_USERNAME', 'root');
-        $this->password = env('DB_PASSWORD', 'root');
 
         $this->up(new Manager());
     }
@@ -64,11 +30,11 @@ class Database
         $manager->setAsGlobal();
         $manager->bootEloquent();
         $manager->addConnection([
-            'driver' => $this->driver,
-            'host' => $this->host,
-            'database' => $this->name,
-            'username' => $this->username,
-            'password' => $this->password,
+            'driver' => env('DB_DRIVER', 'mysql'),
+            'host' => env('DB_HOST', '127.0.0.1') . ':' . env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'test'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'root'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
